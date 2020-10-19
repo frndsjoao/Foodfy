b6ef7c1315c424cbe840c6054d647fee8287c270
João Pedro Assunção <jfernandees002@gmail.com>
2020-10-19 01:27:42 -0300

    Adjustments on layout

--- public/adminScripts.js
+++ public/adminScripts.js
@@ -1,72 +1,40 @@
-/*-- ADMIN PAGES --*/
 function addPreparation() {
-  const preparations = document.querySelector('#preparations');
-  const fieldContainer = document.querySelectorAll('.preparation');
+    const preparations = document.querySelector('#addpreparations')
+    const fieldContainer = document.querySelector('.preparation')
 
-  const newField = fieldContainer[fieldContainer.length -1].cloneNode(true);
+    const newField = fieldContainer[fieldContainer.length -1].cloneNode(true)
 
-  if(newField.children[0].value == "") return false;
+    if(newField.children[0].value == '') return false
 
-  newField.children[0].value = "";
-  preparations.appendChild(newField);
+    newField.children[0].value == ''
+    preparations.appendChild(newField)
 }
 
 function addIngredient() {
-  const ingredients = document.querySelector('#ingredients');
-  const fieldContainer = document.querySelectorAll('.ingredient');
+    const ingredients = document.querySelector('#ingredients')
+    const fieldContainer = document.querySelector('.ingredient')
 
-  const newField = fieldContainer[fieldContainer.length -1].cloneNode(true);
+    const newField = fieldContainer[fieldContainer.length -1].cloneNode(true)
 
-  if(newField.children[0].value == "") return false;
+    if(newField.children[0].value == '') return false
 
-  newField.children[0].value = "";
-  ingredients.appendChild(newField);
+    newField.children[0].value == ''
+    ingredients.appendChild(newField)
 }
 
 document
-  .querySelector('.add-ingredient')
-  .addEventListener('click', addIngredient);
-  
+    .querySelector('.add-ingredient')
+    .addEventListener('click', addIngredient)
+
 document
-  .querySelector('.add-prepare')
-  .addEventListener('click', addPreparation);
+    .querySelector('.add-preparation')
+    .addEventListener('click', addPreparation)
 
 
+function confirmation() {
+    var deleteRecipe = confirm("Deseja realmente deletar essa receita?")
 
-function confirmation(){ 
-  var deleteRecipe = confirm("Deseja realmente deletar essa receita?")
-
-  if(deleteRecipe == true){
-    window.location.href = "/admin/recipes"
-  }
+    if(deleteRecipe == true) {
+        window.location.href = '/admin/recipes'
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // function addPreparation() {
-//   const preparations = document.querySelector('#preparations');
-//   const fieldContainer = document.querySelectorAll('.preparation');
-
-//   const newField = fieldContainer[fieldContainer.length -1].cloneNode(true);
-
-//   if(newField.children[0].value == "") return false;
-
-//   newField.children[0].value = "";
-//   preparations.appendChild(newField);
-// }
